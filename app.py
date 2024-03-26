9dbeb5efebb482780dbfffe1d9a2a0feaafd1e0d
ryanzadrozny <ryanzad@umd.edu>
2024-03-26 19:08:45 -0400

    Found a better method from bootstrap to require validity from each field. If a user does not fill out every field they cannot see the thank you page.

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def favorite_course():
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    if request.method == 'POST' and any(request.form.values()):
+    if request.method == 'POST':
         return render_template('contact.html', form_submitted=True)
     else:
         return render_template('contact.html')
