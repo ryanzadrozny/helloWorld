a4aaecbacde005682fda24270e8a54908c818094
ryanzadrozny <ryanzad@umd.edu>
2024-03-26 18:52:31 -0400

    Added an additional condition to the /contact route to make the user stay on the form page if they do not input information.

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def favorite_course():
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    if request.method == 'POST':
+    if request.method == 'POST' and any(request.form.values()):
         return render_template('contact.html', form_submitted=True)
     else:
         return render_template('contact.html')
